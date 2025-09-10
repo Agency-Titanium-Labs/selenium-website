e4991b6b4fdabd6587ae8a4389db48bd8b00dbbf
Antoine Favereau <antoinefavereau45@gmail.com>
2025-09-10 20:36:14 +0200

    feat: enhance button styles and add primary color variables in globals.css

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,11 +1,15 @@
 import { twMerge } from "tailwind-merge";
 
-const buttonDefaultClasses = "px-4 py-2 font-orbitron font-bold cursor-pointer";
+const buttonDefaultClasses =
+  "relative px-4 py-2 font-orbitron font-bold cursor-pointer [clip-path:polygon(20px_0,_100%_0,_100%_100%,_0_100%,_0_12px)] before:content-[''] before:absolute before:inset-[2px] before:bg-primary before:-z-1 before:[clip-path:polygon(18.5px_0,_100%_0,_100%_100%,_0_100%,_0_11px)] before:transition-all before:duration-300";
 
 const buttonVariants = {
   default:
-    "bg-primary text-grey-darkest border border-primary hover:bg-transparent hover:text-primary transition-all duration-300",
-  outline: "bg-primary/15 border border-primary text-grey-lightest",
+    "bg-linear-160 from-primary-lighter/50 via-primary/50 to-primary-dark/50 text-grey-darkest before:inset-0 hover:before:inset-[2px] hover:text-primary hover:before:bg-grey-darker transition-all duration-300",
+  outline:
+    "bg-linear-160 from-primary-lighter/50 via-primary/50 to-primary-dark/50 text-grey-lightest before:bg-primary-darker hover:from-primary-dark/50 hover:via-primary/50 hover:to-primary-lighter/50 transition-all duration-300",
+  transparent:
+    "bg-transparent text-grey-lightest before:bg-transparent hover:bg-grey-darker transition-all duration-300",
 };
 
 export default function Button({
