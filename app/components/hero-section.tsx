aaef3b708e1c9b38acb22b00362c2c3153055ac7
Antoine Favereau <antoinefavereau45@gmail.com>
2025-09-15 21:23:40 +0200

    feat: add background image to hero section

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -1,9 +1,17 @@
 import Button from "./ui/button";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
     <section className="flex flex-col items-center justify-center gap-12 min-h-screen px-4">
-      <h1 className="text-6xl font-bold leading-tight text-center max-w-2xl">
+      <Image
+        src="/top background light.svg"
+        alt=""
+        width={800}
+        height={800}
+        className="absolute top-0 left-0"
+      />
+      <h1 className="relative text-6xl font-bold leading-tight text-center max-w-2xl">
         L&apos;alliage parfait du{" "}
         <span
           className="text-transparent"
@@ -13,7 +21,7 @@ export default function HeroSection() {
         </span>{" "}
         & du <span className="text-primary">dev</span>
       </h1>
-      <div className="flex items-center gap-4">
+      <div className="relative flex items-center gap-4">
         <Button>Nous contacter</Button>
         <Button variant="outline">Proute</Button>
       </div>
