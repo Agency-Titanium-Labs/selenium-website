580aa8576b7d3aa5defae33184ab9fe79dc806c8
Antoine Favereau <antoinefavereau45@gmail.com>
2025-09-16 00:26:23 +0200

    responsive nav & hero

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -39,7 +39,7 @@ export default function HeroSection() {
         height={800}
         className="absolute top-0 left-0"
       />
-      <h1 className="relative text-6xl font-bold leading-tight text-center max-w-2xl">
+      <h1 className="relative text-4xl sm:text-5xl md:text-6xl font-bold leading-tight text-center max-w-sm sm:max-w-lg md:max-w-2xl">
         L&apos;alliage parfait du{" "}
         <span
           className="text-transparent"
@@ -49,7 +49,7 @@ export default function HeroSection() {
         </span>{" "}
         & du <span className="text-primary">dev</span>
       </h1>
-      <div className="relative flex items-center gap-4">
+      <div className="relative flex flex-col sm:flex-row items-center gap-4">
         <Button>Nous contacter</Button>
         <Button variant="outline">Proute</Button>
       </div>
