ef05b17bde1b5e88ae58bff0f268737b8c5bfc87
Antoine Favereau <antoinefavereau45@gmail.com>
2025-09-20 20:02:50 +0200

    feat: enhance hero and team sections with GSAP animations and update SVG assets

--- app/components/hero-section.tsx
+++ app/components/hero-section.tsx
@@ -1,24 +1,23 @@
 "use client";
 
-import Button from "./ui/button";
-import Image from "next/image";
 import { useRef } from "react";
-import { useGSAP } from "@gsap/react";
+import Image from "next/image";
+import Button from "./ui/button";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function HeroSection() {
-  const backgroundLightContainerRef = useRef<HTMLDivElement>(null);
-  const backgroundLightRef = useRef<HTMLImageElement>(null);
+  const backgroundLightTopRef = useRef<HTMLImageElement>(null);
 
   useGSAP(() => {
-    gsap.to(backgroundLightRef.current, {
-      yPercent: 50,
+    gsap.to(backgroundLightTopRef.current, {
+      yPercent: 40,
       ease: "none",
       scrollTrigger: {
-        trigger: backgroundLightContainerRef.current,
+        trigger: backgroundLightTopRef.current,
         start: "top top",
         end: "bottom top",
         scrub: true,
@@ -27,17 +26,14 @@ export default function HeroSection() {
   }, []);
 
   return (
-    <section
-      ref={backgroundLightContainerRef}
-      className="flex flex-col items-center justify-center gap-12 min-h-screen px-4"
-    >
+    <section className="relative flex flex-col items-center justify-center gap-12 min-h-svh px-4">
       <Image
-        ref={backgroundLightRef}
-        src="/top background light.svg"
+        ref={backgroundLightTopRef}
+        src="/background light.svg"
         alt=""
         width={800}
         height={800}
-        className="absolute top-0 left-0 pointer-events-none select-none"
+        className="absolute top-0 left-8 w-1/4 h-auto pointer-events-none select-none blur-[10vw] -z-10"
       />
       <h1 className="relative text-4xl sm:text-5xl md:text-6xl font-bold leading-tight text-center max-w-sm sm:max-w-lg md:max-w-2xl">
         L&apos;alliage parfait du{" "}
