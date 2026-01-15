7cf1151cbc12b724ff86da5d8a346e600c332e4b
antoinefavereau <antoinefavereau45@gmail.com>
2026-01-15 21:07:31 +0100

    add copyright mention

--- app/components/sections/footer.tsx
+++ app/components/sections/footer.tsx
@@ -2,19 +2,22 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex gap-6 justify-end px-8 py-2">
-      <Link
-        href="/legal-documents/legal-notices"
-        className="text-sm text-grey-lighter hover:text-grey-light"
-      >
-        Mentions Légales
-      </Link>
-      <Link
-        href="/legal-documents/privacy-policy"
-        className="text-sm text-grey-lighter hover:text-grey-light"
-      >
-        Politique de confidentialité
-      </Link>
+    <footer className="flex gap-6 justify-between flex-wrap px-8 py-2">
+      <p>Selenium Studio © 2026 - Tous droits réservés</p>
+      <div className="flex gap-6 ml-auto">
+        <Link
+          href="/legal-documents/legal-notices"
+          className="text-sm text-grey-lighter hover:text-grey-light"
+        >
+          Mentions Légales
+        </Link>
+        <Link
+          href="/legal-documents/privacy-policy"
+          className="text-sm text-grey-lighter hover:text-grey-light"
+        >
+          Politique de confidentialité
+        </Link>
+      </div>
     </footer>
   );
 }
